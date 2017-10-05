7ea98da773a6a3ad41fe1e892c6fe4dcdd7c5206
miapodoesfacu <welschen.facundo@gmail.com>
2017-10-05 10:53:27 -0300

    adding more jobs

---
+++ job_3.groovy
@@ -0,0 +1,20 @@
+project_name = "job3"
+repo_url = "https://github.com/fwelschen/jenkins-test-1.git"
+repo_name = "jenkins-test-1"
+
+
+pipelineJob(project_name) {
+    definition {
+      cpsScm{
+        scm{
+          git{
+            remote{
+              name(repo_name)
+              url(repo_url)
+            }
+          }
+          scriptPath("Jenkinsfile")
+         }
+       }
+    }
+}
